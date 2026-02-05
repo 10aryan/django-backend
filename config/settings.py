72755bc3cf057195bcc0d71f0fc9566e3256c2af
Aryan Singh <aryan.npsingh@gmail.com>
2026-02-05 18:08:13 +0530

    manage.py

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = ["django-backend-5veu.onrender.com"]
+
 
 
 # APPLICATIONS
--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.shortcuts import redirect
+
 def root_redirect(request):
     return redirect('/api/')
 
