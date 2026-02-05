4e33feeac91756f691bced50057877f510c6a395
Aryan Singh <aryan.npsingh@gmail.com>
2026-02-05 17:38:45 +0530

    add host

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+
 
 # APPLICATIONS
 INSTALLED_APPS = [
@@ -95,3 +96,4 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
